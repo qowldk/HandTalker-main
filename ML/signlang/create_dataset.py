456b8059417492f18220e32db2980031216e832a
012 <012012012il@gmail.com>
2024-03-24 17:40:37 +0900

    로직_미세_수정__코드_주석_정리

--- ML/signlang/create_dataset.py
+++ ML/signlang/create_dataset.py
@@ -8,15 +8,10 @@ actions = [
     ('만나다', 1),
     ('반갑다', 2)
 ]
-# labels = [
-#     0,
-#     1,
-#     2
-# ]
 
 seq_length = 30
-secs_for_action = 2 # 1.7
-repeat = 2 # 같은 동작 몇 번 학습 횟수
+secs_for_action = 20 # 1.7
+repeat = 1 # 같은 동작 몇 번 학습 횟수
 
 # MediaPipe hands model
 mp_hands = mp.solutions.hands
@@ -41,7 +36,7 @@ while cap.isOpened():
             # cv2.putText(img, f'Waiting for collecting {action.upper()} action...', org=(10, 30), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255, 255, 255), thickness=2)
             print('debug', idx, action)
             cv2.imshow('img', img)
-            cv2.waitKey(3500 if r==0 else 800) # 밀리초 단위
+            cv2.waitKey(2000 if r==0 else 800) # 밀리초 단위
 
             start_time = time.time()
 
@@ -54,34 +49,44 @@ while cap.isOpened():
                 result = hands.process(img)
                 img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
 
-                if result.multi_hand_landmarks is not None:
+                if result.multi_hand_landmarks is None: continue
+                if len(result.multi_hand_landmarks) == 2:                    
+                    h = 0 # 손 두개 감지 로직을 위한 임시 값
+                    d1 = np.empty(0)
+                    d2 = np.empty(0)
                     for res in result.multi_hand_landmarks:
+                        h+=1
                         joint = np.zeros((21, 4))
                         for j, lm in enumerate(res.landmark):
                             joint[j] = [lm.x, lm.y, lm.z, lm.visibility]
 
-                        # Compute angles between joints
+                        # 각 관절의 벡터 계산
                         v1 = joint[[0,1,2,3,0,5,6,7,0,9,10,11,0,13,14,15,0,17,18,19], :3] # Parent joint
                         v2 = joint[[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20], :3] # Child joint
                         v = v2 - v1 # [20, 3]
-                        # Normalize v
+                        # 정규화 (크기 1의 단위벡터로)
                         v = v / np.linalg.norm(v, axis=1)[:, np.newaxis]
 
-                        # Get angle using arcos of dot product
+                        # 내적의 arcos으로 각도 계산
+                        ### 벡터 a 와 벡터 b 를 내적(dot product)하면 [벡터 a의 크기] × [벡터 b의 크기] × [두 벡터가 이루는 각의 cos값] 이 된다.
+                        ### 그런데 바로 위에서 벡터들의 크기를 모두 1로 표준화시켰으므로 두 벡터의 내적값은 곧 [두 벡터가 이루는 각의 cos값]이 된다.
+                        ### 따라서 이것을 코사인 역함수인 arccos에 대입하면 두 벡터가 이루는 각이 나오게 된다.                        
                         angle = np.arccos(np.einsum('nt,nt->n',
                             v[[0,1,2,4,5,6,8,9,10,12,13,14,16,17,18],:], 
                             v[[1,2,3,5,6,7,9,10,11,13,14,15,17,18,19],:])) # [15,]
 
-                        angle = np.degrees(angle) # Convert radian to degree
-
+                        angle = np.degrees(angle) # 라디안 -> 도
                         angle_label = np.array([angle], dtype=np.float32)
-                        angle_label = np.append(angle_label, idx)
+                        if h==1:
+                            d1 = np.concatenate([joint.flatten(), angle_label[0]])
+                        else:
+                            d2 = np.concatenate([joint.flatten(), angle_label[0], [idx]])
 
-                        d = np.concatenate([joint.flatten(), angle_label])
-
-                        data.append(d)
-
+                        # 파이썬 실행 화면(웹캠)에 랜드마크 그림
                         mp_drawing.draw_landmarks(img, res, mp_hands.HAND_CONNECTIONS)
+                    
+                    d=np.concatenate([d1, d2])
+                    data.append(d)
 
                 cv2.imshow('img', img)
                 if cv2.waitKey(1) == ord('q'):
@@ -92,7 +97,10 @@ while cap.isOpened():
         # file_name = 'C:\\Users\\user\\Downloads\\HandTalker-main_\\ML\\signlang\\dataset\\raw_' + str(action) + '_' + str(created_time)
         # np.save(file_name, data)
         
-        # Create sequence data
+        if len(data) - seq_length < 5:
+            print("2개의 손이 감지된 프레임이 너무 적어 데이터 생성에 실패했습니다.")
+            continue
+        # 시퀀스 데이터 생성
         full_seq_data = []
         for seq in range(len(data) - seq_length):
             full_seq_data.append(data[seq:seq + seq_length])
--- ML/signlang/main.py
+++ ML/signlang/main.py
@@ -37,7 +37,7 @@ def process_frame(image_data):
 
 def mediapipe_detection(image, model):
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # COLOR CONVERSION BGR 2 RGB
-    image.flags.writeable = False  # 이미지 수정 불가
+    image.flags.writeable = False  # 이미지 수정 불가 (결과 왜곡 방지?)
     results = model.process(image)  # 모델을 사용해 입력 이미지에 대한 예측 수행
     image.flags.writeable = True  # 이미지 다시 수정가능
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)  # COLOR COVERSION RGB 2 BGR
@@ -96,16 +96,19 @@ async def handle_client(websocket, path):
             message = await websocket.recv()
             # print("debug: message")
             if message != "":
-
                 # 프레임(이미지) 전처리
                 frame = process_frame(message)
-
                 # mediapipe 감지
                 image, result = mediapipe_detection(frame, hands)
                 # print(f"result: {results}")
-
-            if result.multi_hand_landmarks is not None:
+                
+            if result.multi_hand_landmarks is None: continue
+            if len(result.multi_hand_landmarks) == 2:  
+                h = 0 # 손 두개 감지 로직을 위한 임시 값
+                d1 = np.empty(0)
+                d2 = np.empty(0)
                 for res in result.multi_hand_landmarks: # 감지된 손의 수만큼 반복
+                    h+=1
                     joint = np.zeros((21, 4))
                     for j, lm in enumerate(res.landmark):
                         joint[j] = [lm.x, lm.y, lm.z, lm.visibility] # visibility: 신뢰도 (0~1)
@@ -114,98 +117,106 @@ async def handle_client(websocket, path):
                     v1 = joint[[0,1,2,3,0,5,6,7,0,9,10,11,0,13,14,15,0,17,18,19], :3] # Parent joint
                     v2 = joint[[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20], :3] # Child joint
                     v = v2 - v1 # [20, 3]
-                    # 정규화
+                    # 정규화 (크기 1의 단위벡터로)
                     v = v / np.linalg.norm(v, axis=1)[:, np.newaxis]
 
-                    # 내적 arcos으로 각도 계산
+                    # 내적의 arcos으로 각도 계산
+                    ### 벡터 a 와 벡터 b 를 내적(dot product)하면 [벡터 a의 크기] × [벡터 b의 크기] × [두 벡터가 이루는 각의 cos값] 이 된다.
+                    ### 그런데 바로 위에서 벡터들의 크기를 모두 1로 표준화시켰으므로 두 벡터의 내적값은 곧 [두 벡터가 이루는 각의 cos값]이 된다.
+                    ### 따라서 이것을 코사인 역함수인 arccos에 대입하면 두 벡터가 이루는 각이 나오게 된다.
                     angle = np.arccos(np.einsum('nt,nt->n',
                         v[[0,1,2,4,5,6,8,9,10,12,13,14,16,17,18],:], 
                         v[[1,2,3,5,6,7,9,10,11,13,14,15,17,18,19],:])) # [15,]
 
-                    angle = np.degrees(angle) # Convert radian to degree
-
-                    d = np.concatenate([joint.flatten(), angle])
-
-                    seq.append(d)
-                    # print("debug2", len(seq))
-
-
-                    dc+=1 
-                    print(dc, "debug1.seq크기:", len(seq), end = "__")
-
-                    if len(seq) < seq_length: # 시퀀스 최소치가 쌓인 이후부터 판별
-                        continue
+                    angle = np.degrees(angle) # 라디안 -> 도
+                    angle_label = np.array([angle], dtype=np.float32)
+                    if h==1:
+                        d1 = np.concatenate([joint.flatten(), angle_label[0]])
+                    else:
+                        d2 = np.concatenate([joint.flatten(), angle_label[0]])
+                
+                d=np.concatenate([d1, d2])
+                # print("what???",len(d))
+                seq.append(d)
+                # print("debug2", len(seq))
 
 
-                    if len(seq)>seq_length*100:  # 과도하게 쌓임 방지
-                        seq=seq[-seq_length:]
-                    
-                    # 시퀀스 데이터를 신경망 모델에 입력으로 사용할 수 있는 형태로 변환
-                    input_data = np.expand_dims(np.array(seq[-seq_length:], dtype=np.float32), axis=0)
+                dc+=1 
+                print(dc, "debug1.seq크기:", len(seq))
 
-                    y_pred = model.predict(input_data).squeeze() # 각 동작에 대한 예측 결과 (각각의 확률)
-
-                    i_pred = int(np.argmax(y_pred)) # 최댓값 인덱스: 예측값이 가장 높은 값(동작)의 인덱스
-                    conf = y_pred[i_pred] # 가장 확률 높은 동작의 확률이
-
-                    if conf < 0.9:   # 90% 이상일 때만 수행
-                        continue
-
-                    # debug
-                    print("debug2.예측동작인덱스:", i_pred)
+                if len(seq) < seq_length: # 시퀀스 최소치가 쌓인 이후부터 판별
+                    continue
 
 
-                    action = actions[i_pred]
-                    ####
-                    action_seq.append(action)
+                if len(seq)>seq_length*100:  # 과도하게 쌓임 방지
+                    seq=seq[-seq_length:]
+                
+                # 시퀀스 데이터를 신경망 모델에 입력으로 사용할 수 있는 형태로 변환
+                input_data = np.expand_dims(np.array(seq[-seq_length:], dtype=np.float32), axis=0)
 
-                    if len(action_seq) < 3:
-                        continue
+                y_pred = model.predict(input_data).squeeze() # 각 동작에 대한 예측 결과 (각각의 확률)
 
-                    this_action = ''
-                    if action_seq[-1] == action_seq[-2] == action_seq[-3]:
-                        this_action = action
-                    ####
-                    # debug
-                    print(dc, "debug3.예측동작(출력동작):", this_action)
+                i_pred = int(np.argmax(y_pred)) # 최댓값 인덱스: 예측값이 가장 높은 값(동작)의 인덱스
+                conf = y_pred[i_pred] # 가장 확률 높은 동작의 확률이
 
-                    seq=[]
-                    
-                    
-                    # print("DEBUG", this_action)
-                    # if this_action !='':      
-                    #     sentence.append(this_action)
-                    # if len(sentence) > sentence_length:
-                    #     sentence = sentence[-sentence_length:]
-                        # print(' '.join(sentence))
+                if conf < 0.9:   # 90% 이상일 때만 수행
+                    continue
 
-                    print("send?1")
-                    # if time.time() - extra_time_start < extra_time or action=='':  # 데이터 전달 최소 텀
-                    if time.time() - extra_time_start < extra_time or this_action=='':  # 데이터 전달 최소 텀
-                        continue
-                    extra_time_start = time.time()
-                    print("send?2")
+                # debug
+                print("debug2.예측동작인덱스:", i_pred)
 
 
-                    # print("왜멈춰?1 ", action)
-                    # print("왜멈춰?2 ", previous, action, previous==action)
+                action = actions[i_pred]
+                ####
+                action_seq.append(action)
 
-                    if previous == this_action: continue  # 중복 전달 회피  ???
-                    previous = this_action
-                    # if previous == action: continue  # 중복 전달 회피  ???
-                    # previous = action
-                    print("보냄?3")
+                if len(action_seq) < 3:
+                    continue
+
+                this_action = ''
+                if action_seq[-1] == action_seq[-2] == action_seq[-3]:
+                    this_action = action
+                ####
+                # debug
+                print(dc, "debug3.예측동작(출력동작):", this_action)
+
+                seq=[]
+                
+                
+                # print("DEBUG", this_action)
+                # if this_action !='':      
+                #     sentence.append(this_action)
+                # if len(sentence) > sentence_length:
+                #     sentence = sentence[-sentence_length:]
+                    # print(' '.join(sentence))
+
+                print("send?1")
+                # if time.time() - extra_time_start < extra_time or action=='':  # 데이터 전달 최소 텀
+                if time.time() - extra_time_start < extra_time or this_action=='':  # 데이터 전달 최소 텀
+                    continue
+                extra_time_start = time.time()
+                print("send?2")
 
 
-                    result_dict = {'result': this_action}
-                    # result_dict = {'result': action}
-                    result_json = json.dumps(result_dict)
+                # print("왜멈춰?1 ", action)
+                # print("왜멈춰?2 ", previous, action, previous==action)
 
-                    try:
-                        if websocket.open:
-                            await websocket.send(result_json)
-                    except Exception as e:
-                        print(f"send error: {str(e)}")
+                if previous == this_action: continue  # 중복 전달 회피  ???
+                previous = this_action
+                # if previous == action: continue  # 중복 전달 회피  ???
+                # previous = action
+                print("send?3")
+
+
+                result_dict = {'result': this_action}
+                # result_dict = {'result': action}
+                result_json = json.dumps(result_dict)
+
+                try:
+                    if websocket.open:
+                        await websocket.send(result_json)
+                except Exception as e:
+                    print(f"send error: {str(e)}")
                     
     except websockets.exceptions.ConnectionClosedOK:
         pass
