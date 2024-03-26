2db4fc6a3acd578b0caf4ed975a36e55b244600b
nazero <skdud4081@gmail.com>
2024-03-26 23:33:44 +0900

    modify footer

--- front/src/components/Footer/Footer.tsx
+++ front/src/components/Footer/Footer.tsx
@@ -1,7 +1,8 @@
-import logo from "../../assets/icons/handtalker.webp";
+//import logo from "../../assets/icons/handtalker.webp";
 import { AiFillGithub } from "react-icons/ai";
 import devocean from "../../assets/icons/footer_devocean.webp";
 import { useLocation } from "react-router-dom";
+import newLogo from "../../assets/icons/icon.webp";
 
 const Footer = () => {
   const location = useLocation();
@@ -9,39 +10,22 @@ const Footer = () => {
 
   return (
     <div
-      className={`bg-[#23A352] w-full ${
+      className={`bg-[#1F2D21] w-full ${
         path === "/" ? "fixed bottom-[0px]" : "relative"
       } h-32 flex flex-row items-center justify-center`}
     >
-      <img src={logo} alt="logo" className="w-auto h-20 md:h-32" />
+      <img src={newLogo} alt="logo" className="w-auto h-20 md:h-32" />
       <div className="flex flex-col items-start justify-start ml-[30px]">
-        <p className="font-main text-[rgb(61,125,71)] text-xs md:text-sm">
-          ©2023 HandTalker
+        <p className="font-main text-white text-xs md:text-sm">
+          ©2024 Computer Science and Engineering
         </p>
-        <p className="font-main text-[rgb(61,125,71)] text-xs md:text-sm">
-          DEVOCEAN YOUNG 2nd 404 Team
+        <p className="font-main text-white text-xs md:text-sm">
+           SENIER PROJECT 
         </p>
-        <p className="font-main text-[rgb(61,125,71)] text-xs md:text-sm">
-          곽수진, 송보경, 정승준, 지현빈
+        <p className="font-main text-white text-xs md:text-sm">
+          김영일, 서영광, 배지아, 장나영
         </p>
-        <div className="flex flex-row items-center justify-center mt-[5px] md:mt-[10px]">
-          <a
-            href="https://github.com/DEVOCEAN-YOUNG-404/HandTalker"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <p className="text-[rgb(61,125,71)] text-[23px] md:text-[30px] mr-[5px]">
-              <AiFillGithub />
-            </p>
-          </a>
-          <a href="https://devocean.sk.com/" target="_blank" rel="noreferrer">
-            <img
-              src={devocean}
-              alt="devocean"
-              className="w-[20px] md:w-[27px]"
-            />
-          </a>
-        </div>
+     
       </div>
     </div>
   );
